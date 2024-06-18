115ea2830401df3eb23c4042cd5ff81d140b13ce
Liam Simmons <liam.simmons@powerhrg.com>
2024-06-18 14:31:51 -0400

    Add child routes from app and home.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,32 +1,28 @@
-import { useState, useEffect } from "react";
-import UserCard from "../components/UserCard";
-import NavBar from "../components/NavBar";
+import { useState, useEffect } from "react"
+import UserCard from "../components/UserCard"
+import { Outlet } from "react-router-dom"
 
 function Home() {
-  const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState([])
 
-  useEffect(() =>{
+  useEffect(() => {
     fetch("http://localhost:4000/users")
-      .then(r => r.json())
-      .then(data => setUsers(data))
-      .catch(error => console.error(error));
-  }, []);
-  
-  const userList = users.map(user =>{
-    return <UserCard key={user.id} user={user}/>;
-  });
+      .then((r) => r.json())
+      .then((data) => setUsers(data))
+      .catch((error) => console.error(error))
+  }, [])
+
+  const userList = users.map((user) => {
+    return <UserCard key={user.id} user={user} />
+  })
 
   return (
-    <>
-      <header>
-        <NavBar />
-      </header>
-      <main>
-        <h1>Home!</h1>
-        {userList}
-      </main>
-    </>
-  );
-};
+    <main>
+      <h1>Home!</h1>
+      <Outlet />
+      {userList}
+    </main>
+  )
+}
 
-export default Home;
+export default Home
